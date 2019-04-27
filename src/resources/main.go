1d0fe217799818553d9e9b3c95789299b978ba48
Matt Simpson <m.simpson@outlook.com>
2019-04-27 22:32:04 +1200

    update go to use SQS

--- src/resources/main.go
+++ src/resources/main.go
@@ -1,29 +1,63 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/sqs"
 )
 
-// Handler is executed by AWS Lambda in the main function. Once the request
-// is processed, it returns an Amazon API Gateway response object to AWS Lambda
+//Message body for batch
+type Message struct {
+	Batch int `json:"batch"`
+}
+
+var sess = session.Must(session.NewSessionWithOptions(session.Options{
+	SharedConfigState: session.SharedConfigEnable,
+}))
+
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	//index, err := ioutil.ReadFile("public/index.html")
-
-	evt := events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello World",
-		Headers: map[string]string{
-			"Content-Type": "text/html",
-		},
+	var msg Message
+	err := json.Unmarshal([]byte(request.Body), &msg)
+	if err != nil {
+		fmt.Println("There was an error:", err)
 	}
 
-	// if err != nil {
-	// 	return events.APIGatewayProxyResponse{}, err
-	// }
+	svc := sqs.New(sess)
 
-	return evt, nil
+	qURL := "https://sqs.ap-southeast-2.amazonaws.com/632298589294/mjsdemo-sqs"
+
+	var res = []*sqs.SendMessageOutput{}
+
+	for index := 0; index < msg.Batch; index++ {
+		result, err := svc.SendMessage(&sqs.SendMessageInput{
+			DelaySeconds: aws.Int64(10),
+			MessageBody: aws.String("Go...go...go"),
+			QueueUrl:    &qURL,
+		})
+		res = append(res, result)
+		if err != nil {
+			fmt.Println("Error", err)
+		}
+	}
+
+	if err != nil {
+        fmt.Println("Error", err)
+        return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Failed"),
+			StatusCode: 500,
+		}, nil
+	}
+	
+	return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Job Accepted: %s", string(len(res))),
+			StatusCode: 200,
+		}, nil
+
 }
 
 func main() {
--- src/resources/main_test.go
+++ src/resources/main_test.go
@@ -8,18 +8,20 @@ import (
 
 func TestHandler(t *testing.T) {
 
-	request := events.APIGatewayProxyRequest{}
+	request := events.APIGatewayProxyRequest{
+		Body: "{\"batch\":10}",
+	}
 	expectedResponse := events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
-		Body: "Hello World",
+		Body: "Job Accepted:",
 	}
 
 	response, err := Handler(request)
 
-	assert.Equal(t, response.Headers, expectedResponse.Headers)
+	//assert.Equal(t, response.Headers, expectedResponse.Headers)
 	assert.Contains(t, response.Body, expectedResponse.Body)
 	assert.Equal(t, err, nil)
 
